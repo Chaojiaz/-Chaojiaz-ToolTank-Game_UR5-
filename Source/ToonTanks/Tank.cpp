dc0bb5dde5338aabdfe274928bb37f544ec37675
Chaojiaz <chaojiaz@andrew.cmu.edu>
2022-08-08 00:35:50 -0400

    Add move forward rotation for tank pawn

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -5,7 +5,7 @@
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
 #include "Kismet/GameplayStatics.h"
-
+// Constructor
 ATank::ATank()
 {
   SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
@@ -21,6 +21,7 @@ void ATank::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
   Super::SetupPlayerInputComponent(PlayerInputComponent);
 
   PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+  PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
 }
 
 void ATank::Move(float Value)
@@ -28,5 +29,14 @@ void ATank::Move(float Value)
   FVector DeltaLocation = FVector::ZeroVector;
   // X = Value * DeltaTime * Speed
   DeltaLocation.X = Value * Speed * UGameplayStatics::GetWorldDeltaSeconds(this);
-  AddActorLocalOffset(DeltaLocation);
+  AddActorLocalOffset(DeltaLocation, true);
+  // UE_LOG(LogTemp, Display, TEXT("The value is: %f"), Value);
+}
+
+void ATank::Turn(float Value)
+{
+  FRotator DeltaRotation = FRotator::ZeroRotator;
+  // Yaw  = Values * DeltaTime * TurnRate
+  DeltaRotation.Yaw = Value * TurnRate * UGameplayStatics::GetWorldDeltaSeconds(this);
+  AddActorLocalRotation(DeltaRotation, true);
 }
